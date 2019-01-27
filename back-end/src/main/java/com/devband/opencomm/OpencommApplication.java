881f139c7d243cc13ac3cdd6880673795eda3c6c
kylee <kylee@devband.com>
2019-01-27 21:15:52 +0900

    create spring boot project

---
+++ back-end/src/main/java/com/devband/opencomm/OpencommApplication.java
@@ -0,0 +1,14 @@
+package com.devband.opencomm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpencommApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpencommApplication.class, args);
+    }
+
+}
+
---
+++ back-end/src/test/java/com/devband/opencomm/OpencommApplicationTests.java
@@ -0,0 +1,17 @@
+package com.devband.opencomm;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OpencommApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
